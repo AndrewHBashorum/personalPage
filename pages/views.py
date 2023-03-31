18de97d16dff3d37fb86c333bd8d7209ef917626
andrewhbashorm <c00238900@itcarlow.ie>
2023-03-31 14:14:04 +0100

    Added nav bar, image etc. No text yet

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 
+# Create your views here.
+
 def home(request):
     return render(request, 'pages/home.html')
 
-# Create your views here.
+def lanu(request):
+    return render(request, 'pages/lanu.html')
+
+def blog(request):
+    return render(request, 'pages/blog.html')
+
+def youtube(request):
+    return render(request, 'pages/youtube.html')
+
+def gallery(request):
+    return render(request, 'pages/gallery.html')
+
+def models(request):
+    return render(request, 'pages/models.html')
+
+def research(request):
+    return render(request, 'pages/research.html')
